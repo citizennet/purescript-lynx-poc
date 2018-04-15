2e35c5c01ab1667b84692f44f9e8d338ef52fc2e
Thomas R. Honeyman <admin@thomashoneyman.com>
2018-04-14 21:18:16 -0700

    Added function handling within record example

--- app/Main.purs
+++ app/Main.purs
@@ -7,8 +7,10 @@ import Control.Monad.Eff (Eff)
 import Halogen.Aff as HA
 import Halogen.VDom.Driver (runUI)
 import Lynx.Dynamic.Component as Component
+import Lynx.Dynamic.GraphRecord as GraphRecord
 
 main :: ∀ eff. Eff (HA.HalogenEffects (Component.Effects eff)) Unit
 main = HA.runHalogenAff do
   body <- HA.awaitBody
+  let x = GraphRecord.built
   runUI (Component.component Signup.handleValidation Signup.renderInput Signup.handleRelation) unit body
